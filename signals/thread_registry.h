b00c9a8fba8ca8702e0ebf3782d901aa13cf5632
Dan Moodie <dtmoodie@gmail.com>
2016-02-08 00:56:26 -0500

    missing export.

--- signals/thread_registry.h
+++ signals/thread_registry.h
@@ -3,9 +3,10 @@
 #include <map>
 #include <mutex>
 #include <vector>
+#include <signals/Defs.h>
 namespace Signals
 {
-    class thread_registry
+    class SIGNAL_EXPORTS thread_registry
     {
         std::map<int, std::vector<boost::thread::id>> _thread_map;
         thread_registry();
@@ -16,5 +17,4 @@ namespace Signals
         virtual boost::thread::id get_thread(int type);
         static thread_registry* get_instance();
     };
-
 }
