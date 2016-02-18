cc650882a47476dcb9165821c0146de9741aba9f
Dan <dtmoodie@gmail.com>
2016-02-18 10:42:49 -0500

    more logging functions.

--- signals/logging.hpp
+++ signals/logging.hpp
@@ -2,4 +2,6 @@
 #include <boost/log/trivial.hpp>
 
 #define LOG(severity) BOOST_LOG_TRIVIAL(severity) << "[" << __FUNCTION__ << "] "
-#define DOIF(condition, expression, severity) if(condition) { LOG(severity) << #condition << " is true, thus performing " << #expression; expression;}
+#define DOIF(condition, expression, severity) if(condition) { LOG(severity) << #condition << " is true, thus performing " << #expression; expression;} else { LOG(severity) << #condition << " failed";}
+#define DOIF_LOG_PASS(condition, expression, severity) if(condition) { LOG(severity) << #condition << " is true, thus performing " << #expression; expression;} 
+#define DOIF_LOG_FAIL(condition, expression, severity) if(condition) { expression; } else { LOG(severity) << "Unable to perform " #expression " due to " #condition << " failed";}
